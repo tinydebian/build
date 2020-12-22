87afebba6c67e127b8e29d2153157f868a057ff2
tech at tinyDebian <tech@tinydebian.com>
2020-12-22 13:39:54 +0800

    Add bootstrap and customize rootfs (Debian 10 buster)

---
+++ build_debian.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Preparation:
+# Run this script in Ubuntu 18.04.x
+# sudo apt install qemu-user-static debootstrap binfmt-support debian-archive-keyring
+
+# Environment Variables that are needed to be configured before running this script:
+# Debian_ARCH        (default: arm64, aka aarch64)
+# Debian_Release     (default: buster)
+# Debian_rootfs_dir  (default: out/debian_rootfs)
+
+# Input parameters:
+# $1:
+#     bootstrap: use debootstrap to create new rootfs.
+#     customize: chrooot an existing rootfs to make changes.
+
+set -e
+
+readonly CUR_DIR=`pwd`
+readonly OUT_DIR=$CUR_DIR/../out
+readonly TARGET_DIR=$OUT_DIR/debian_rootfs
+readonly DISTRO=buster
+readonly MIRROR=${MIRROR:-http://ftp.cn.debian.org/debian/}
+
+show_usage() {
+	echo "usage: $0 bootstrap"
+	echo "       $0 customize"
+}
+
+bootstrap() {
+	echo
+	echo "--- Bootstrap rootfs (1st stage) ..."
+	echo
+
+	sudo rm -rf $TARGET_DIR
+	sudo debootstrap --arch=arm64 --foreign $DISTRO $TARGET_DIR $MIRROR
+
+	echo
+	echo "--- Bootstrap rootfs (2nd stage) ..."
+	echo
+
+	# for debootstrap second_stage
+	sudo cp /usr/bin/qemu-aarch64-static $TARGET_DIR/usr/bin/
+	sudo LANG=C.UTF-8 LANGUAGE==C.UTF-8 LC_ALL=C.UTF-8 chroot $TARGET_DIR /debootstrap/debootstrap --second-stage --no-check-gpg $MIRROR
+}
+
+customize() {
+	echo
+	echo "--- Customize rootfs ..."
+	echo
+
+	sudo mkdir -p $TARGET_DIR/tmp/
+	sudo cp chroot_customize.sh $TARGET_DIR/tmp/
+
+	sudo LANG=C.UTF-8 LANGUAGE==C.UTF-8 LC_ALL=C.UTF-8 chroot $TARGET_DIR /tmp/chroot_customize.sh
+	sudo rm -f $TARGET_DIR/tmp/chroot_customize.sh
+}
+
+parse_args_and_run() {
+	if [ $# -ne 1 ]; then
+		show_usage
+		exit 1
+	fi
+
+	case "$1" in
+		bootstrap)
+			bootstrap
+			;;&
+		bootstrap | customize)
+			customize
+			;;
+		*)
+			show_usage
+			exit 1
+			;;
+	esac
+}
+
+mkdir -p $OUT_DIR
+parse_args_and_run $@
+
+exit 0
---
+++ chroot_customize.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+##############################
+# we are inside chroot
+
+set -e
+
+install_packages() {
+	apt update
+	apt -y install openssh-server net-tools resolvconf
+	apt -y install less zip unzip parted dosfstools usbutils
+	apt -y install sudo man vim git htop bmon
+	apt -y install python3
+	apt -y install libnl-3-dev libnl-genl-3-dev
+	apt -y install batctl
+}
+
+customize_debian() {
+	# Debian9 strech: enable ssh root login
+	sed -i 's/\#PermitRootLogin prohibit\-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+	# Debian 8 jessie: enable ssh root login
+	#sed -i 's/PermitRootLogin without\-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+	echo tinyDebian > /etc/hostname
+
+	echo root:tinyDebian | chpasswd
+
+	# misc
+	echo "" >> /root/.bashrc
+	echo "alias ll='ls -alF'"  >> /root/.bashrc
+}
+
+install_packages
+customize_debian
+
+# exit chroot
+exit 0
